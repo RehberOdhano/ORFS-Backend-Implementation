4383bf8a8bbf09669b6612ecc4c05222866cc9be
RehberOdhano <rehber.odhano30@gmail.com>
2022-10-03 21:14:59 +0500

    implemented a function in analytics routes

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -1,6 +1,8 @@
 // MODELS
 const Customer = require("../models/customer");
 const User = require("../models/user");
+const SP = require("../models/serviceProvider");
+const Complainee = require("../models/complainee");
 
 /*
 =============================================================================
@@ -43,6 +45,70 @@ exports.getUserCount = (req, res) => {
           status: 200,
           success: true,
           count: count,
+        });
+      }
+    });
+  } catch (error) {
+    console.log("ERROR: " + error.message);
+  }
+};
+
+// this'll return percentage of registered users, count of serviceproviders
+// and complainees respectively...
+exports.getUserAnalytics = (req, res) => {
+  try {
+    const company_id = req.params.id;
+    var analytics = {};
+    User.count({ company_id: company_id }, (err, count) => {
+      if (err) {
+        res.send({
+          status: 500,
+          success: false,
+          message: err.message,
+        });
+      } else {
+        const totalUsers = count;
+        User.count({
+          company_id: company_id,
+          status: { $ne: "UNREGISTERED" },
+        }).exec((err, count) => {
+          if (err) {
+            res.send({
+              status: 500,
+              success: false,
+              message: err.message,
+            });
+          } else {
+            analytics.registeredUsers =
+              totalUsers != 0 ? (count / totalUsers) * 100 : 0;
+            SP.count({ company_id: company_id }, (err, count) => {
+              if (err) {
+                res.send({
+                  status: 500,
+                  success: false,
+                  message: err.message,
+                });
+              } else {
+                analytics.serviceproviders = count;
+                Complainee.count({ company_id: company_id }, (err, count) => {
+                  if (err) {
+                    res.send({
+                      status: 500,
+                      success: false,
+                      message: err.message,
+                    });
+                  } else {
+                    analytics.complainees = count;
+                    res.send({
+                      status: 200,
+                      success: true,
+                      data: analytics,
+                    });
+                  }
+                });
+              }
+            });
+          }
         });
       }
     });
--- routes/analytics-routes.js
+++ routes/analytics-routes.js
@@ -16,5 +16,9 @@ const analyticsController = require("../controllers/analyticsController");
 // ANALYTICS ROUTES
 analytics_controller.get("/customers", analyticsController.getCustomerCount);
 analytics_controller.get("/users", analyticsController.getUserCount);
+analytics_controller.get(
+  "/user-analytics/:id",
+  analyticsController.getUserAnalytics
+);
 
 module.exports = analytics_controller;
