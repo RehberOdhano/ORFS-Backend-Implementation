ba823e97124c55505a54b7095aa8cb25d0e319bd
RehberOdhano <rehber.odhano30@gmail.com>
2022-10-05 22:16:15 +0500

    removed bugs...

--- models/rating.js
+++ models/rating.js
@@ -4,7 +4,6 @@ const ratingSchema = mongoose.Schema(
   {
     company_id: {
       type: mongoose.Schema.Types.ObjectId,
-<<<<<<< HEAD
       ref: "Customer",
     },
     complainee_id: {
@@ -18,21 +17,6 @@ const ratingSchema = mongoose.Schema(
     complaint_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Complaint",
-=======
-      ref: "Customer"
-    },
-    complainee_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Complainee"
-    },
-    serviceprovider_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "ServiceProvider"
-    },
-    complaint_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Complaint"
->>>>>>> 7fb886ded24ba9e27b31ab9887b71e71ea63da73
     },
     rating_level: {
       type: Number,
