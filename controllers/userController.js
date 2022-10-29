26bd71950b7bdcdb6e9ad811406b53399afba2bd
Raafaye Faheem <spa45rky@gmail.com>
2022-10-29 15:41:27 +0500

    fixed get complaints with user object

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,14 @@ exports.getAllComplaints = (req, res) => {
     const id = req.params.id;
     Complaint.find({ user_id: id })
       .populate("category")
-      .populate("assignedTo")
+      // .populate("assignedTo")
+      .populate({
+        path: 'assignedTo',
+        populate: {
+          path: 'user_id',
+          model: 'User'
+        }
+      })
       .exec((err, complaints) => {
         if (err) {
           res.send({
