0d1b8c33e7d9cba7cc9bbfc75d577e3f2a8cb942
RehberOdhano <rehber.odhano30@gmail.com>
2022-12-01 14:22:53 +0500

    updated email-worker.js

--- utils/email-worker.js
+++ utils/email-worker.js
@@ -7,7 +7,7 @@ const sendEmail = require("../utils/email");
 // name of the queue
 const queue = "email-queue";
 
-// connecting to the RabbitMQ server
+// STEP-01: connecting to the RabbitMQ server
 const open = amqp.connect(process.env.AMQP_SERVER);
 
 // publisher
@@ -15,12 +15,13 @@ const open = amqp.connect(process.env.AMQP_SERVER);
 // to set up a task queue. It adds our message to the 'email-queue' queue.
 const publishMessage = (payload) =>
   open
-    .then((connection) => connection.createChannel())
+    .then((connection) => connection.createChannel()) // STEP-02: Create a channel
     .then((channel) =>
       // channe.assertQueue will create a queue if there is no queue in the server
       channel
-        .assertQueue(queue, { durable: false })
+        .assertQueue(queue, { durable: false }) // STEP-03: Assert the queue
         .then(() =>
+          // STEP-04: Send the message to the queue
           channel.sendToQueue(queue, Buffer.from(JSON.stringify(payload)))
         )
     )
@@ -30,14 +31,17 @@ const publishMessage = (payload) =>
 // this function starts up our worker in the background to listen for incoming
 // messages added to our 'email-queue' queue.
 const consumeMessage = () => {
+  // STEP-01: Connect to the RabbitMQ server
   open
-    .then((connection) => connection.createChannel())
+    .then((connection) => connection.createChannel()) // STEP-02: Create a channel
     .then((channel) =>
       channel.assertQueue(queue).then(() => {
+        // STEP-03: Assert the queue
         console.log(
           " [*] Waiting for messages in %s. To exit press CTRL+C",
           queue
         );
+        // STEP-04: Receive messages
         channel.consume(queue, (msg) => {
           console.log(msg);
           if (msg !== null) {
