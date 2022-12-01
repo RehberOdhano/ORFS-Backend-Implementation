7766db04a0f9ec644d27818538932de86959c6b9
RehberOdhano <rehber.odhano30@gmail.com>
2022-12-02 04:00:17 +0500

    removed bug from sendMessage function

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -4,18 +4,29 @@ const Message = require("../models/message");
 exports.sendMessage = async (req, res) => {
   try {
     const { senderId, text, conversationId } = req.body;
-    const newMessage = new Message({
-      conversation_id: conversationId,
-      sender: senderId,
-      message: text,
-    });
-    const savedMessage = await newMessage.save();
-    res.send({
-      status: 200,
-      success: true,
-      message: "MESSAGE IS SUCCESSFULLY SENT!",
-      chatMessage: savedMessage,
-    });
+    Message.create(
+      {
+        conversation_id: conversationId,
+        sender: senderId,
+        message: text,
+      },
+      (err, savedMessage) => {
+        if (err) {
+          res.send({
+            status: 500,
+            success: false,
+            message: err.message,
+          });
+        } else {
+          res.send({
+            status: 200,
+            success: true,
+            message: "MESSAGE IS SUCCESSFULLY SENT!",
+            chatMessage: savedMessage,
+          });
+        }
+      }
+    );
   } catch (err) {
     console.error("ERROR: " + err.message);
     res.send({
