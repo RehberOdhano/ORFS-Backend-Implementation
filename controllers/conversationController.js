71c1cef1a4f65754445b654d9072923915cb6f5f
RehberOdhano <rehber.odhano30@gmail.com>
2022-12-02 02:04:47 +0500

    updated a few features in conversationController...

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -1,18 +1,36 @@
 // MODELS
 const Conversation = require("../models/conversation");
 
-exports.createNewConversation = async (req, res) => {
+exports.createNewConversation = (req, res) => {
   try {
-    const newConversation = new Conversation({
-      memebers: [req.body.senderId, req.body.receiverId],
-    });
+    Conversation.findOne({
+      memebers: { $in: [req.body.senderId, req.body.receiverId] },
+    }).exec(async (err, conversation) => {
+      if (err) {
+        res.send({
+          status: 500,
+          success: false,
+          message: err.message,
+        });
+      } else if (conversation) {
+        res.send({
+          status: 201,
+          success: true,
+          message: "CONVERSATION ALREADY EXISTS!",
+        });
+      } else {
+        const newConversation = new Conversation({
+          memebers: [req.body.senderId, req.body.receiverId],
+        });
 
-    const savedConversation = await newConversation.save();
-    res.send({
-      status: 200,
-      success: true,
-      message: "CONVERSATION IS SUCCESSFULLY CREATED!",
-      conversation: savedConversation,
+        const savedConversation = await newConversation.save();
+        res.send({
+          status: 200,
+          success: true,
+          message: "CONVERSATION IS SUCCESSFULLY CREATED!",
+          conversation: savedConversation,
+        });
+      }
     });
   } catch (err) {
     console.error("ERROR: " + err.message);
@@ -27,23 +45,21 @@ exports.createNewConversation = async (req, res) => {
 exports.getConversation = (req, res) => {
   try {
     const id = req.params.id;
-    Conversation.findOne({ memebers: { $in: [id] } }).exec(
-      (err, conversation) => {
-        if (err) {
-          res.send({
-            status: 500,
-            success: false,
-            message: err.message,
-          });
-        } else {
-          res.send({
-            status: 200,
-            success: true,
-            conversation: conversation,
-          });
-        }
+    Conversation.find({ memebers: { $in: [id] } }).exec((err, conversation) => {
+      if (err) {
+        res.send({
+          status: 500,
+          success: false,
+          message: err.message,
+        });
+      } else {
+        res.send({
+          status: 200,
+          success: true,
+          conversation: conversation,
+        });
       }
-    );
+    });
   } catch (err) {
     console.error("ERROR: " + err.message);
     res.send({
