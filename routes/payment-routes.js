0077867ef3b99b378bbb8a65da2d3c31aa58e0bd
RehberOdhano <rehber.odhano30@gmail.com>
2022-12-26 18:08:30 +0500

    made minor changes

--- routes/payment-routes.js
+++ routes/payment-routes.js
@@ -10,6 +10,6 @@ const paymentController = require("../controllers/paymentController");
 // PAYMENT USING STRIPE - TESTING
 paymentRouter.post("/initiate/payment/:id", paymentController.initiatePayment);
 paymentRouter.post("/paymentIntent", paymentController.addPaymentIntent);
-paymentRouter.put("/confirm/payment", paymentController.confirmPayment);
+paymentRouter.put("/confirm/payment/:id", paymentController.confirmPayment);
 
 module.exports = paymentRouter;
